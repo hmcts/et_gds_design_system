6cdf4a3e05c68d38d0a1646c4a17831f8856c3c3
Gary Taylor <gary.taylor@hismessages.com>
2026-04-09 17:07:24 +0100

    Added guard to only initialize once

--- app/javascript/controllers/gds_page_controller.js
+++ app/javascript/controllers/gds_page_controller.js
@@ -4,8 +4,12 @@ import Components from "../components";
 
 export default class extends Controller {
   connect() {
+    if (document.body.dataset.gdsPageInitialised === "true") return;
+
+    document.body.dataset.gdsPageInitialised = "true";
     document.body.classList.add("js-enabled");
     document.body.classList.add("gds-enabled");
+    document.body.classList.add("govuk-frontend-supported");
     initAll();
     Components.RevealOnRadioButton.init();
   }
