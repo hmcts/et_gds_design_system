4ff7ee8d690f1300ee997a4c2d8030d08a1885f4
Gary Taylor <gary.taylor@hismessages.com>
2026-04-09 12:35:19 +0100

    Helper changes needed after isolation

--- lib/et_gds_design_system/engine.rb
+++ lib/et_gds_design_system/engine.rb
@@ -5,6 +5,13 @@ module EtGdsDesignSystem
 
     config.et_gds_design_system = ::Rails::Application::Configuration::Custom.new
     config.et_gds_design_system.api_url = 'http://api.et.127.0.0.1.nip.io:3100/api'
+
+    initializer 'et_gds_design_system.helpers' do
+      ActiveSupport.on_load(:action_controller_base) do
+        helper EtGdsDesignSystem::FileDropzoneHelper
+      end
+    end
+
     initializer 'et_gds_design_system.assets' do |app|
       app.config.assets.paths << Rails.root.join('node_modules', 'govuk-frontend', 'dist', 'govuk', 'assets')
     end
